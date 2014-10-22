4b1f8abdc6ef5fe6dea77b7b51d7fe39a957f302
Ed Carrel <edward@carrel.org>
2014-10-22 02:23:31 -0700

    Fixing the Baler integration test to get along with Rubinius.

--- test/integration/baler_test.rb
+++ test/integration/baler_test.rb
@@ -18,16 +18,26 @@ class Baler::IntegrationTest < Test::Unit::TestCase
     include Hay::Task
 
     def initialize(params = {})
+      raise if params.length == 0
+      @processed = false
+      @params = params
     end
 
-    @@runs = 0
-
-    def self.runs
-      @@runs
-    end
+    attr_reader :params
+    attr_reader :processed
 
     def process(foo)
-      @@runs += 1
+      @processed = true
+    end
+  end
+
+  class MockResolver
+    def initialize(task)
+      @task = task
+    end
+
+    def build
+      @task
     end
   end
 
@@ -39,35 +49,99 @@ class Baler::IntegrationTest < Test::Unit::TestCase
     include Hay::Route
   end
 
+
   class MockConsumer
+    class Factory
+      def initialize(observer)
+        @consumers = []
+        @observer = observer
+      end
+
+      attr_reader :consumers
+
+      def new(agent)
+        t = MockConsumer.new(agent,@observer)
+        @consumers << t
+        t
+      end
+    end
+
+    class Observer
+      def initialize
+        @mutex = Mutex.new
+        @latch = ConditionVariable.new
+        @tasks = []
+      end
+
+      attr_reader :tasks
+
+      def wait
+        @mutex.synchronize {
+          @latch.wait(@mutex)
+        }
+      end
+
+      def <<(task)
+        @tasks << task
+        if @tasks.length == 10
+          @latch.signal
+        end
+      end
+    end
     def tasks
       [MockTask]
     end
 
-    def consumed
+    def initialize(agent, observer)
+      @observer = observer
+      super(agent)
+    end
 
+    attr_reader :observer
+
+    def push(task)
+      resolved_task = Hay::Task::Resolver.new(task)
+      @observer << resolved_task
+      @queue.push(resolved_task)
+      @queue.run
     end
 
     include Hay::Consumer
   end
 
   test "thing" do
-    begin
-      Timeout::timeout(1) do
-        publisher = Baler::Publisher.new(MockRoute)
-        10.times do 
-          message = OpenStruct.new(:payload => {'name' => 'mock_task'}, :destination => MockRoute)
-          publisher.publish(message)
-        end
+    Thread.abort_on_exception = true
 
-        consumer = Baler::Consumer.new(MockConsumer)
-        consumer.listen
-      end
-      fail
-    rescue Timeout::Error
-      if MockTask.runs != 10
-        fail
-      end
+
+    params = 10.times.map do |x|
+      {'task' => {'iteration' => x}}
     end
+
+    publisher = Baler::Publisher.new(MockRoute)
+
+    params.each do |p|
+      message = OpenStruct.new(payload: {'name' => 'mock_task', 'task' => p}, :destination => MockRoute)
+      publisher.publish(message)
+    end
+
+    @observer = MockConsumer::Observer.new
+    @consumer_factory = MockConsumer::Factory.new(@observer)
+
+    runner = Thread.new do
+      consumer = Baler::Consumer.new(@consumer_factory)
+      consumer.listen
+    end
+
+    @observer.wait
+
+    observed_tasks = @observer.tasks
+
+    assert_equal observed_tasks.length, 10
+    observed_tasks.each { |t| 
+      assert t.kind_of?(Hay::Task::Decorator) 
+      assert t.__getobj__.kind_of?(MockTask) 
+      assert t.processed
+    }
+    assert_equal Set.new(observed_tasks.map(&:params)), Set.new(params)
   end
 end
